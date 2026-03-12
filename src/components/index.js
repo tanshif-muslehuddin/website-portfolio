f58de526ef8e535e13d57234d4432fc9fb06f3b4
Helyxxx <tmuslehuddin@hotmail.com>
2026-03-12 15:27:23 -0400

    added a hoc to manage window stats and added first window -terminal

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@
 import NavBar from "#components/NavBar.jsx";
 import Welcome from "#components/Welcome.jsx";
 import Dock from "#components/Dock.jsx";
+import WindowControls from "#components/WindowControls.jsx";
 
-export { NavBar, Welcome, Dock };
+export { NavBar, Welcome, Dock, WindowControls };
